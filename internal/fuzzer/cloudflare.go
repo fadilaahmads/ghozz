681034e359cd3411477dbe7c2a1b03f2a30572fe
Fadila Ahmad S. <fadilahmad59@gmail.com>
2025-02-10 23:24:54 +0700

    Add boolean checking if cloudflare detected

--- internal/fuzzer/cloudflare.go
+++ internal/fuzzer/cloudflare.go
@@ -7,17 +7,22 @@ import (
   "github.com/PuerkitoBio/goquery"
 )
 
-func ExtractCloudflareHtml(body []byte){
+func ExtractCloudflareHtml(body []byte) (bool, error) {
+  var detected bool
   // detect if the response is actually cloudflare page
   doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
   if err != nil {
     fmt.Errorf("Error parsing HTML: %v", err)
-    return
+    return false, err
   }
 
   title := doc.Find("title").Text() 
   if(title == "Attention Required! | Cloudflare"){
+    detected = true
     fmt.Println("[!] Cloudflare Detected!")
     fmt.Println("[X] Unable to proceed!")
+  } else {
+    detected = false
   }
+  return detected, nil
  }
