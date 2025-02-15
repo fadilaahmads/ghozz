c047ddffa00229b94518b9d05a099a8abec32c96
Fadila Ahmad S. <fadilahmad59@gmail.com>
2025-02-15 23:14:34 +0700

    add fuzz unit test with tor

--- internal/fuzzer/fuzz_test.go
+++ internal/fuzzer/fuzz_test.go
@@ -5,17 +5,35 @@ import (
   "net/http"
   "net/http/httptest"
   "testing"
+  "ghozz/internal/tor"
 )
 
 func TestFuzz(t *testing.T) {
+  // Mock HTTP server
   mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
-    io.WriteString(w, "Mock Response")
+    io.WriteString(w, "Mock response")
   }))
-
   defer mockServer.Close()
 
+  // Simulate wordlist
   wordlist := []string{"test", "admin", "login"}
 
-  Fuzz(mockServer.URL, wordlist, nil, "")
+  // Use mock HTTP client
+  client := mockServer.Client()
+
+  // Call Fuzz with updated parameters (client and no TOR transport)
+  Fuzz(mockServer.URL, wordlist, client, nil, "")
+}
+
+func TestFuzzTor(t *testing.T) {
+  transport, err := tor.SetupTOR()
+  if err != nil {
+    t.Fatalf("Failed to setup TOR service: %v", err)
+  }
+
+  var wordlist = []string{"hidden_service", "secret"}
+  
+  var target string = "http://check.torproject.org"
+  Fuzz(target, wordlist, nil, transport, "")
 }
