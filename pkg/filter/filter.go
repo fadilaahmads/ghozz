5dff8fb9ce9e33ca52284b20ce1029bdcdefe23e
Fadila Ahmad S. <fadilahmad59@gmail.com>
2025-02-16 19:07:58 +0700

    create http response code filter

---
+++ pkg/filter/filter.go
@@ -0,0 +1,32 @@
+package filter
+
+import (
+  "fmt"
+  "strings"
+)
+
+func ParseHideCodes(flag string) (map[int]bool, error) {
+  hiddenCodes := make(map[int]bool)
+  invalidCodes := []string{}
+  
+  if flag == "" {
+    return nil, fmt.Errorf("Empty codes")
+
+  }
+
+  for _, code := range strings.Split(flag, ",") {
+    var c int
+    _, err := fmt.Sscanf(code, "%d", &c)
+    if err != nil {
+      invalidCodes = append(invalidCodes, code)
+    } else {
+      hiddenCodes[c] = true
+    }
+  }
+
+  if len(invalidCodes) > 0 {
+    return hiddenCodes, fmt.Errorf("invalid status codes provided: %v", strings.Join(invalidCodes, ", "))
+  }
+   
+  return hiddenCodes, nil
+}
